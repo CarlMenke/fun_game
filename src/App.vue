8bfdfa5744b0fbac833bcb925268d1e7a33a101d
carlmenke <carlmenke13@gmail.com>
2022-10-31 16:52:03 -0700

    initalized into vue repository

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue';
+  export default {
+    name: 'App',
+    components : {
+      HelloWorld
+    },
+    data : function () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    created: function() {
+
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+ 
